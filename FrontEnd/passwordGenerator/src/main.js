de64b2f3ab144c96c446e719da7b0ca6b4abbe69
Kaique Oliveira <kaiquekurosaki@hotmail.com>
2023-06-22 11:26:05 -0300

    making the initial steps for the logic

---
+++ FrontEnd/passwordGenerator/src/main.js
@@ -0,0 +1,12 @@
+// importing librarie
+import { nanoid } from 'nanoid';
+import copy from 'clipboard-copy';
+
+// importing css
+import './style.css';
+
+// cathing the DOM
+const button = document.getElementById('button');
+const password = document.getElementById('password');
+const reset = document.getElementById('reset');
+const copyBtn = document.getElementById('copy');
